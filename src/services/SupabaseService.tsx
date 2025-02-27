2580ae9045ed65741cad0a031f04cdc22af5898f
Alexandre M <Alexandre.molesti@hotmail.com>
2025-02-27 14:47:32 +0100

    Intégration du service de base Supabase, pour se connecter avec son API il faut avoir un .env

--- src/services/SupabaseService.tsx
+++ src/services/SupabaseService.tsx
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "";
-const supabaseKey = "";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY || "";
+
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase URL or Key in environment variables');
 }
