72137e2eca36796033a6a96e16a3dcd853ec1fd3
Alexandre M <Alexandre.molesti@hotmail.com>
2025-05-06 11:23:58 +0200

    Ajoute capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.boddyrp',
+  appName: 'boddyrp',
+  webDir: 'www'
+};
+
+export default config;
